4f1e6b90a9909f3c0b895a1a05deab33a1a883d9
fengde <derek.feng@mkcorp.com>
2019-03-20 13:33:23 +0800

    change comapre method to get

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'compare' => 'home#compare'
+  get 'compare' => 'home#compare'
 
   root 'songs#index'
 end
